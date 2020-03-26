541fc066f78baa54d1d7d37be4efb2946d94b6c4
Marko Lozančić <lozamarko@hotmail.com>
2020-03-26 17:35:41 +0100

    Updates

---
+++ precache-manifest.245db2648ec705922cb3979c685cdebb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f4a34f997fb9eefc9f5352705cf7123",
+    "url": "/robot-rolodex/index.html"
+  },
+  {
+    "revision": "ca60ae8a5f2e11486de4",
+    "url": "/robot-rolodex/static/css/main.a260da57.chunk.css"
+  },
+  {
+    "revision": "0f05fbb2bb76b0455248",
+    "url": "/robot-rolodex/static/js/2.62cea425.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robot-rolodex/static/js/2.62cea425.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca60ae8a5f2e11486de4",
+    "url": "/robot-rolodex/static/js/main.c72479ff.chunk.js"
+  },
+  {
+    "revision": "90af0f053e16dbe1b490",
+    "url": "/robot-rolodex/static/js/runtime-main.5ad58d7d.js"
+  }
+]);
